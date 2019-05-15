0672a68a90b526f27182c4d90e6c6dff153db993
Joe Buhlig <joe@joebuhlig.com>
2019-05-15 10:27:36 -0500

    FIX: user faraday for api call

--- plugin.rb
+++ plugin.rb
@@ -3,25 +3,16 @@
 # author: ProCourse
 # url: https://github.com/procourse/procourse-convertkit
 
-gem 'mime-types-data', '3.2019.0331'
-gem 'mime-types', '3.2.2'
-gem 'httparty', '0.16.4'
 require 'uri'
+require "faraday"
 
 after_initialize do
   DiscourseEvent.on(:user_created) do |user|
-    url = "https://api.convertkit.com/v3/forms/#{SiteSetting.procourse_convertkit_form_id}/subscribe?api_key=#{SiteSetting.procourse_convertkit_api_key}&email=#{URI::encode(user.email)}&first_name=#{URI::encode(user.name)}"
-    request_payload = {
-        headers: {
-          'Content-Type' => 'application/json'
-        }
-      }
-    begin
-      byebug
-      response = HTTParty.send(:post, url, request_payload)
-    rescue HTTParty::Error => e
-      puts e.message
-      byebug
-    end
+    url = "https://api.convertkit.com/v3/forms/#{SiteSetting.procourse_convertkit_form_id}/subscribe"
+    conn = Faraday.new(url)
+    conn.params['api_key'] = SiteSetting.procourse_convertkit_api_key
+    conn.params['email'] = user.email
+    conn.params['first_name'] = user.name
+    response = conn.post
   end
 end
