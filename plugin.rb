16e30d1731dddf3d89960a981cbd58b22eaf5459
Joe Buhlig <joe@joebuhlig.com>
2019-05-15 13:19:34 -0500

    FEATURE: add users to groups

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,7 @@
 
 require 'uri'
 require "faraday"
+require 'json'
 
 after_initialize do
   DiscourseEvent.on(:user_created) do |user|
@@ -14,5 +15,28 @@ after_initialize do
     conn.params['email'] = user.email
     conn.params['first_name'] = user.name
     response = conn.post
+
+    if SiteSetting.procourse_convertkit_add_group && SiteSetting.procourse_convertkit_group && SiteSetting.procourse_convertkit_tag
+      body = JSON.parse(response.body)
+      tag = SiteSetting.procourse_convertkit_tag
+      subscriber_id = body["subscription"]["subscriber"]["id"]
+      tag_url = "https://api.convertkit.com/v3/subscribers/#{subscriber_id}/tags"
+      tag_conn = Faraday.new(url)
+      tag_conn.params['api_key'] = SiteSetting.procourse_convertkit_api_key
+      tag_response = tag_conn.get(tag_url)
+
+      tag_body = JSON.parse(tag_response.body)
+      tags = tag_body["tags"]
+
+      for tag in tags
+        if tag["name"] == SiteSetting.procourse_convertkit_tag
+          group = Group.find_by(name: SiteSetting.procourse_convertkit_group)
+          if group && user
+            group.add(user)
+            group.save
+          end
+        end
+      end
+    end
   end
 end
